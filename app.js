59e380fc2ddee1f308d443d027b6ca8db5020502
Ivan <ivanchernetskey@mail.ru>
2021-05-01 20:35:31 +0300

    added mongoose, made async start function, created MongoDB, connected to our Database

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const express = require('express')
 const config = require('config')
+const mongoose = require('mongoose') //подключаем покет mongoose для подключения к базе данных MongoDB
 
 const app = express() //наш будущий сервер
 
 const PORT = config.get('port') || 5000
 
-app.listen(PORT, () => console.log(`App has been started on port:${PORT}...`))
+async function start() {
+    try {
+        //если все хорошо - подключаюсь к монго
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        }) //метод connect возвращает promise - поэтому используем await и пишем все в async функции
+        //вторым параметром мы передаем набор опций
+
+        //после того как база данных подсоидиниться - запускаем сервер
+        app.listen(PORT, () => console.log(`App has been started on port:${PORT}...`))
+
+    } catch(e) {
+        console.log('Server Error ', e.message)
+        process.exit(1)
+        //завершаем процесс в Node.js в случае если что-то пойдет не так благодаря глобальному объекту
+        //process и его методу exit()
+    }
+}
+
+start()
