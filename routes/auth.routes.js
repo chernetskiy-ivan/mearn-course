34052f111b2241680d882e2c6cd51b1f0e16ff11
Ivan <ivanchernetskey@mail.ru>
2021-05-02 18:10:24 +0300

    prepare post requests in auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,6 +2,18 @@ const {Router} = require('express')
 //создаем роут
 const router = Router()
 
+//добавляем post запросы
+// /api/auth/register
+router.post('/register', async (req, res) => {
+
+})
+
+
+// /api/auth/login
+router.post('/login', async (req, res) => {
+
+})
+
 //роут это middleware в express
 //экспортируем объект роута из модуля
 module.exports = router
