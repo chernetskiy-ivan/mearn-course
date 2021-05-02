fbc8e5bda38685ca97766d5026a3b1fc2d9a6bd0
Ivan <ivanchernetskey@mail.ru>
2021-05-02 18:58:01 +0300

    make /register post request

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,7 +6,30 @@ const router = Router() //создаем роут
 //добавляем post запросы
 // /api/auth/register
 router.post('/register', async (req, res) => {
+    try {
+        const {email, password} = req.body
 
+        const candidate = await User.findOne({email: email})
+
+        //проверка есть ли уже такой пользователь
+        if(candidate) {
+            return res.status(400).json({message: 'Такой пользователь уже существует'})
+        }
+
+        //регистрируем нового полльзователя
+        //хэширую пароль
+        const hashedPassword = await bcrypt.hash(password, 12)
+        const user = new User({email, password: hashedPassword})
+
+        //схраняем пользователя
+        await user.save()
+
+        //отвечаем фронтенду
+        res.status(201).json({message: 'Пользователь создан'})
+
+    } catch(e) {
+        res.status(500).json({message: 'Что-то пошло не так, попробуйте снова'})
+    }
 })
 
 
