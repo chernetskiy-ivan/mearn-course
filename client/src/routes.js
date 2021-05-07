781151e134307a7bb481b7d5e6e6485348f90884
Ivan <ivanchernetskey@mail.ru>
2021-05-07 19:30:01 +0300

    we process NOT AUTHENTICATED case

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import {LinksPage} from "./pages/LinksPage";
 import {CreatePage} from "./pages/CreatePage";
 import {DetailPage} from "./pages/DetailPage";
+import {AuthPage} from "./pages/AuthPage";
 
 export const useRoutes = isAuthenticated => {
     if(isAuthenticated) {
@@ -14,16 +15,20 @@ export const useRoutes = isAuthenticated => {
                 <Route path="/create" exact>
                     <CreatePage />
                 </Route>
-                <Route path="/detail/:id" exact>
+                <Route path="/detail/:id">
                     <DetailPage />
                 </Route>
+                <Redirect to="/create" />
             </Switch>
         )
     }
 
     return (
         <Switch>
-
+            <Route path="/" exact>
+                <AuthPage />
+            </Route>
+            <Redirect to="/" />
         </Switch>
     )
 }
