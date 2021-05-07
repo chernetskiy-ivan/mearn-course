f243aecd5d218579c9415ed67125028079ee0ad8
Ivan <ivanchernetskey@mail.ru>
2021-05-07 19:20:54 +0300

    added Routes in routes.js

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,11 +1,22 @@
 import React from 'react'
-import { Switch } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
+import {LinksPage} from "./pages/LinksPage";
+import {CreatePage} from "./pages/CreatePage";
+import {DetailPage} from "./pages/DetailPage";
 
 export const useRoutes = isAuthenticated => {
     if(isAuthenticated) {
         return (
             <Switch>
-
+                <Route path="/links" exact>
+                    <LinksPage />
+                </Route>
+                <Route path="/create" exact>
+                    <CreatePage />
+                </Route>
+                <Route path="/detail/:id" exact>
+                    <DetailPage />
+                </Route>
             </Switch>
         )
     }
