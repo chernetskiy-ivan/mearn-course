c8df2515ef7a7f38dafaf717ed51cc2011c3e868
Ivan <ivanchernetskey@mail.ru>
2021-05-10 21:28:22 +0300

    make message.hook

---
+++ client/src/hooks/message.hook.js
@@ -0,0 +1,10 @@
+import {} from 'react'
+
+export const useMessage = () => {
+    return useCallback(text => {
+        if(window.M && text) {
+            //toast метод из materialize
+            window.M.toast({ html: 'Text'})
+        }
+    }, [])
+}
