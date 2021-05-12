1d8226e6bf999843d3e0939b8e8d3a5715dd5997
Ivan <ivanchernetskey@mail.ru>
2021-05-12 21:09:49 +0300

    added styles in Navbar component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -12,8 +12,8 @@ export const Navbar = () => {
     }
     return (
         <nav>
-            <div className="nav-wrapper">
-                <a href="/" className="brand-logo">Сокращение ссылок</a>
+            <div className="nav-wrapper blue darken-1" style={{ padding: '0 2rem' }}>
+                <span className="brand-logo">Сокращение ссылок</span>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li><NavLink to='/create'>Создать</NavLink></li>
                     <li><NavLink to='/links'>Ссылки</NavLink></li>
