8782e6f68be0a85be56ce9f391305e74786781f2
Ivan <ivanchernetskey@mail.ru>
2021-05-14 17:35:14 +0300

    make CreatePage component

--- client/src/pages/CreatePage.js
+++ client/src/pages/CreatePage.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, {useEffect, useState, useContext} from 'react'
+import {useHttp} from '../hooks/http.hooks'
+import {AuthContext} from "../context/AuthContext";
+import {useHistory} from "react-router-dom";
 
 export const CreatePage = () => {
+    const history = useHistory()
+    const auth = useContext(AuthContext)
+    const {request} = useHttp()
+    const [link, setLink] = useState('')
+
+    useEffect(() => {
+        window.M.updateTextFields()
+    }, [])
+
+    const pressHandler = async event => {
+        if(event.key === 'Enter') {
+            try {
+                const data = await request('/api/link/generate', 'POST', {from: link}, {Authorization: `Bearer ${auth.token}`})
+                //делаю редирект
+                history.push(`/detail/${data.link._id}`)
+            } catch (e) {}
+        }
+    }
+
     return (
-        <div>
-            <h1>Create Page</h1>
+        <div className="row">
+            <div className="col s8 offset-s2" style={{padding: '2rem'}}>
+                <div className="input-field">
+                    <input
+                        placeholder="Вставте ссылку"
+                        id="link"
+                        type="text"
+                        value={link}
+                        onChange={e => setLink(e.target.value)}
+                        onKeyPress={pressHandler}
+                    />
+                    <label htmlFor="link">Введите ссылку</label>
+                </div>
+            </div>
         </div>
     )
 }
