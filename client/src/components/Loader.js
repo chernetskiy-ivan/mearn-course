fd33350f0ee43220602bc74655528ce48a345739
Ivan <ivanchernetskey@mail.ru>
2021-05-16 13:21:23 +0300

    make DetailPage

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -19,5 +19,4 @@ export const Loader = () => {
             </div>
         </div>
     )
-
 }
--- client/src/pages/DetailPage.js
+++ client/src/pages/DetailPage.js
@@ -1,10 +1,37 @@
-import React, {useState} from 'react'
+import React, {useState, useCallback, useContext, useEffect} from 'react'
+import {useParams} from 'react-router-dom'
+import {useHttp} from "../hooks/http.hooks"
+import {AuthContext} from "../context/AuthContext"
+import {Loader} from "../components/Loader";
 
 export const DetailPage = () => {
-    //const [link, setLink] = useState(null)
+    //использую токен потому что его добавляю в зависимости
+    const {token} = useContext(AuthContext)
+    const {request, loading} = useHttp()
+    const [link, setLink] = useState(null)
+    const linkId = useParams().id
+
+    const getLink = useCallback(async () => {
+        try {
+            const fetched = await request(`/api/link/${linkId}`, 'GET', null, {
+                Authorization: `Bearer ${token}`
+            })
+            setLink(fetched)
+        } catch (e) {}
+    },[token, linkId, request])
+
+    useEffect(() => {
+        getLink()
+    }, [getLink])
+
+    if(loading) {
+        return <Loader />
+    }
+
     return (
-        <div>
-            <h1>Detail Page</h1>
-        </div>
+        <>
+            //если не loading и есть link то показываем компонент LinkCard
+            {!loading && link && <LinkCard /> }
+        </>
     )
 }
