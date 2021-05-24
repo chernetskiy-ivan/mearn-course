7a3efa324da5b4fb76418bb39e89770d9922a723
Ivan <ivanchernetskey@mail.ru>
2021-05-24 20:55:51 +0300

    added production working way for NODE_ENV

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const config = require('config')
+const path = require('path')
 const mongoose = require('mongoose') //подключаем пакет mongoose для подключения к базе данных MongoDB
 
 const app = express() //наш будущий сервер
@@ -12,6 +13,15 @@ app.use(express.json({extended: true}))
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/link', require('./routes/link.routes'))
 app.use('/t',require('./routes/redirects.routes'))
+
+if(process.env.NODE_ENV === "production") {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+    //обработка всех get запросов
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 
 const PORT = config.get('port') || 5000
 
